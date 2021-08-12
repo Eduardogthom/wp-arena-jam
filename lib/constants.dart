2339ab3878581e13d00bafe132f2e53189c398ff
Eduardo Gabriel Thomas <eduardogthom@gmail.com>
2021-08-12 19:01:24 -0300

    feat: add colors contants

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class AppColors {
+  // Component colors
+  static const kGray = Color(0xFFC4C4C4);
+  static const kBlack = Color(0xFF18181C);
+  static const kWhite = Color(0xFFFFFFFF);
+
+  // Logo colors
+  static const kBlue = Color(0xFF00E1FF);
+  static const kPink = Color(0xFFFF0098);
+
+  // Support
+  static const kLink = Color(0xFF91623A);
+}
