9e28c13e54ca6307bba6cd156cfcc138ad67e6b9
Leonardo Burati <leonardoburati@hotmail.com>
2021-08-14 21:42:31 -0300

    feat: sign in page

---
+++ lib/screens/sign_in_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:wp_arena_flutter/components/dropdown.dart';
+import 'package:wp_arena_flutter/components/input.dart';
+import 'package:wp_arena_flutter/components/submit_button.dart';
+import 'package:wp_arena_flutter/constants.dart';
+
+class SignInScreen extends StatefulWidget {
+  static String id = 'sign_in_screen';
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _selectedGender = "Outro";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kBlack,
+        elevation: 0,
+        title: Text(
+          "Cadastro de usuário",
+          style: TextStyle(color: kWhite),
+        ),
+        leading: BackButton(
+          color: kWhite,
+        ),
+      ),
+      backgroundColor: kBlack,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: SafeArea(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Row(
+                            children: [
+                              Column(
+                                children: [
+                                  CircleAvatar(
+                                    foregroundImage:
+                                        AssetImage('images/users/user_6.png'),
+                                    radius: 45.0,
+                                  ),
+                                  TextButton(
+                                    onPressed: () => {},
+                                    child: Text("Alterar avatar"),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Input(
+                  enabled: true,
+                  hintText: 'Email',
+                  errorMessage: 'Por favor, digite seu email.',
+                  prefixIcon: Icons.email,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Input(
+                  enabled: true,
+                  hintText: 'Data de nascimento',
+                  errorMessage: 'Por favor, digite sua data de nascimento.',
+                  prefixIcon: Icons.calendar_today,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Input(
+                  enabled: true,
+                  hintText: 'Senha',
+                  errorMessage: 'Por favor, digite sua senha.',
+                  prefixIcon: Icons.lock,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Dropdown(
+                  selectedGender: _selectedGender,
+                  onChanged: ((newValue) {
+                    setState(() => {_selectedGender = newValue.toString()});
+                  }),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: SubmitButton(
+                    formKey: _formKey,
+                    labelText: 'CADASTRAR',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
