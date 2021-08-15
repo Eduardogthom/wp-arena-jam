a77b96c871f555ec2e42b2b655658803511fc78a
Leonardo Burati <leonardoburati@hotmail.com>
2021-08-15 17:53:24 -0300

    feat: add avatar button component

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wp_arena_flutter/screens/login_screen.dart';
 import 'package:wp_arena_flutter/screens/profile_screen.dart';
+import 'package:wp_arena_flutter/screens/schedule_match_screen.dart';
 import 'package:wp_arena_flutter/screens/sign_in_screen.dart';
 
 import 'screens/home_screen.dart';
@@ -14,13 +15,14 @@ class WPArena extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignInScreen(),
-      initialRoute: SignInScreen.id,
+      home: ScheduleMatchScreen(),
+      initialRoute: ScheduleMatchScreen.id,
       routes: {
         HomeScreen.id: (context) => HomeScreen(),
         LoginScreen.id: (context) => LoginScreen(),
         SignInScreen.id: (context) => SignInScreen(),
-        ProfileScreen.id: (context) => ProfileScreen()
+        ProfileScreen.id: (context) => ProfileScreen(),
+        ScheduleMatchScreen.id: (context) => ScheduleMatchScreen(),
       },
     );
   }
